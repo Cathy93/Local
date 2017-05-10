f3898c2a9aeba7be82d5d71e1223c353f60958ad
Cathy93 <CathyMarion93@gmail.com>
2017-05-10 18:34:40 +1000

    associate prosucts with users

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "default.png"
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: {greater_than: 0}
+
+  belongs_to :user
 end
